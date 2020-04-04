76b3a72f18e781b086e9aba2b46200562157c33b
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-04-04 17:04:29 +0530

    sanitiser

---
+++ sanitizer/request.php
@@ -0,0 +1,102 @@
+<?php 
+	// var_dump($_POST);
+	// exit();
+ 	 $lname = $_POST["san-last-name"];
+ 	 $fname  = $_POST["san-your-name"];
+ 	 $address= $_POST["san-address"];
+	 $address2 = $_POST["san-address-line-2"];
+	 $city = $_POST["san-city"];
+ 	 $state  = $_POST["san-menu-state"];
+ 	 $country= $_POST["san-billing_country"];
+	 $zip = $_POST["san-number-744"];
+	 $phone = $_POST["san-number-13"];
+ 	 $size  = $_POST["san-menu-739"];
+ 	 $msg= $_POST["san-textarea-887"];
+	 // $location = $_POST["location"];
+
+	 $subject ="Hand Sanitizer Request By" . $fname . "" . $lname;
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'From: ASP CARES <' . $fname .">" ."\r\n";
+	
+	// $headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
+	// $headers .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+	
+	   	$message= "<table border='0' cellpadding='4' cellspacing='4' width='100%'>
+
+	   			  <tr><td style='font-size:1.3em;' colspan='2'><strong>Contact Details</strong></td></tr>
+	   			   <tr>
+	                   <td align='left' width='35%'><strong>Name :</strong></td>
+	                   <td align='left' width='60%'>
+	                   ".  $fname . " " . $lname ."</td>
+	                 </tr>
+
+	                  <tr>
+	                   <td align='left' width='35%'><strong>Address:</strong></td>
+	                   <td align='left' width='60%'>
+	                   ".  $address . " " . $address2 ."</td>
+	                 </tr>
+
+
+	   			  	<tr>
+	                   <td align='left' width='35%'><strong>City :</strong></td>
+	                   <td align='left' width='60%'>
+	                   ". $city ."</td>
+	                 </tr>
+
+
+	                 <tr>
+	                   <td align='left' width='35%'><strong>State :</strong></td>
+	                   <td align='left' width='60%'>
+	                   ".  $state ."</td>
+	                 </tr>
+
+	                  <tr>
+	                   <td align='left' width='35%'><strong>Country:</strong></td>
+	                   <td align='left' width='60%'>
+	                   ".  $country ."</td>
+	                 </tr>
+
+
+	   			  	<tr>
+	                   <td align='left' width='35%'><strong>Zip :</strong></td>
+	                   <td align='left' width='60%'>
+	                   ". $zip ."</td>
+	                 </tr>
+
+	   			  	<tr>
+	                   <td align='left' width='35%'><strong>Phone :</strong></td>
+	                   <td align='left' width='60%'>
+	                   ". $phone ."</td>
+	                 </tr>
+
+	                 	   			   <tr>
+	                   <td align='left' width='35%'><strong>Size :</strong></td>
+	                   <td align='left' width='60%'>
+	                   ".  $size ."</td>
+	                 </tr>
+
+	                  <tr>
+	                   <td align='left' width='35%'><strong>Message :</strong></td>
+	                   <td align='left' width='60%'>
+	                   ".  $msg ."</td>
+	                 </tr>
+
+	               </table>";  
+	              
+	  	  if(mail("content@gmicro.us", $subject, $message, $headers))
+			// if(mail("roxanetlowry@gmail.com,seo@gmicro.us", $subject, $message, $headers))
+	  	  {
+			// delete the cookie so it cannot sent again by refreshing this page
+			setcookie('tntcon','');
+			echo "Your enquiry has been sent successfully"; 
+		}
+		// else{
+		// 	echo "error";
+		// }
+
+		
+	  	//$msg = "Your enquiry has been sent successfully.";
+	  	
+	
+?>
